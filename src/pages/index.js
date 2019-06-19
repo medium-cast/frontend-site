1a48b5be3d13743a514cdbbfaad01de4edff1da4
Xin Wei Ding <xind1cl@gmail.com>
2019-06-19 16:47:26 +1000

    slick

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,49 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-
+import Slider from "react-slick";
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
+import img1 from "../../content/assets/1.jpg"
+import img2 from "../../content/assets/2.jpg"
+import img3 from "../../content/assets/3.jpg"
 
 class BlogIndex extends React.Component {
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
     const posts = data.allMarkdownRemark.edges
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      lazyLoad: true,
+      variableWidth: true,
+      adaptiveHeight: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    };
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="All posts" />
-        <Bio />
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <div key={node.fields.slug}>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </div>
-          )
-        })}
-      </Layout>
+      <div style={{overflowX:"hidden"}}>
+        <Slider {...settings} style={{width:"100%"}}>
+          <div>
+            <img style={{width:"100vw", height: "100vh", objectFit: "cover"}} src={img1} alt="Play" />
+            <h1 style={{display: "block", width:"100%", textAlign: "center", position:"absolute"}}>Play</h1>
+          </div>
+          <div>
+            <img style={{width:"100vw", height: "100vh", objectFit: "cover"}} src={img2} alt="Share" />
+          </div>
+          <div>
+            <img style={{width:"100vw", height: "100vh", objectFit: "cover"}} src={img3} alt="Experience" />
+          </div>
+        </Slider>
+        <SEO title="contact" />
+      </div>
+
     )
   }
 }
